470b5c19f611b6c4a2601dd863cce938b9f005cd
PurpleSPC <108835910+PurpleSPC@users.noreply.github.com>
2022-08-15 07:57:31 -0400

    beginning schema and resolver designs

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,5 +1,7 @@
 const { models } = require("mongoose");
 
 module.exports = {
-
+    asset: (parent, args) =>{
+        return assets.find(asset => asset.id === args.id)
+    },
 };
--- src/schema.js
+++ src/schema.js
@@ -2,17 +2,69 @@ const {gql} = require('apollo-server');
 
 //createss a gql schema
 module.exports = gql`
+enum AssetStatus {
+    AVAILABLE
+    ON_CASE
+    DOWN_RESTOCK
+    DOWN_BACKORDER
+    PROBLEM
+}
+type Asset {
+    assetId: ID!
+    assetVendor: String!
+    assetName: String!
+    #for tray#,ERC#, etc. perhaps rename?
+    assetReference: String!
+    assetLocation: String!
+    assetStatus: AssetStatus!
+}
 type Case {
-    id: ID!
-    dos: String!
-    Hospital: String!
+    caseId: ID!
+    caseSurgeon: String!
+    caseFacility: String!
+}
+type Request {
+    requestId: ID!
+    requestTeam: String!
+    requestAssetVendor: String!
+    requestAssetName: String!
+    requestShipLocation: String!
+    requestNotes: [String!]
+    #this is nullable so that null= not reserved or a reserveId is stored once reserve is created
+    requestIsReserved: String
+}
+type Reserve {
+    reserveId: ID!
+    reserveTeam: String!
+    reserveAssetVendor: String!
+    reserveAssetName: String!
+    reserveShipLocation: String!
+    reserveNotes: [String!]
+    #this is nullable so that null= not reserved or a reserveId is stored once reserve is created
+    reserveIsShipped: String
+}
+type Shipment {
+    Id: ID!
+    Team: String!
+    AssetVendor: String!
+    AssetName: String!
+    ShipLocation: String!
+    Notes: [String!]
+    #this is nullable so that null= not reserved or a reserveId is stored once reserve is created
+    requestIsReserved: String
 }
 
 type Query{
-    cases: [Case!]!
+    cases: [Case!]!,
+    assets: [Asset!]!
 }
 
 type Mutation {
-    newCase(dos: String!, hospital:String!): Case!
+    newCase(caseDate: String!, caseFacility:String!, caseSurgeon: String!): Case!,
+    newAsset(assetVendor: String!, 
+        assetName: String!, 
+        assetReference: String!, 
+        assetLocation: AssetStatus!,
+        assetStatus: String!): Asset!
 }
 `;
